d2a427fa083fe9dfadcbbf3dae9171f7eb882f4b
maisvendoo <maisvendoo@yandex.ru>
2023-09-20 01:01:58 +0300

    First empty build

--- include/vsgSFML/keyboard.hpp
+++ include/vsgSFML/keyboard.hpp
@@ -5,7 +5,14 @@
 
 namespace vsgSFML
 {
+    class KeyboardMap : public vsg::Inherit<vsg::Object, KeyboardMap>
+    {
+    public:
 
+        KeyboardMap();
+
+        bool getKeySingularly() const;
+    };
 }
 
 #endif
--- src/vsgSFML/keyboard.cpp
+++ src/vsgSFML/keyboard.cpp
@@ -2,12 +2,5 @@
 
 namespace vsgSFML
 {
-    class KeyboardMap : public vsg::Inherit<vsg::Object, KeyboardMap>
-    {
-    public:
 
-        KeyboardMap();
-
-        bool getKeySingularly() const;
-    };
 }
